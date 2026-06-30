980c5b9f81bb72eb0983fd2c3cae7844833ad7a2
TheChupalin <lester.saez14@gmail.com>
2026-06-30 09:20:34 -0400

    a

--- backend-micro-microservicio2/target/generated-sources/annotations/com/microservice/microservicio2/interfaces/IProductoMapperImpl.java
+++ backend-micro-microservicio2/target/generated-sources/annotations/com/microservice/microservicio2/interfaces/IProductoMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-29T17:16:56-0400",
-    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 1.4.200.v20220802-0458, environment: Java 17.0.4.1 (Eclipse Adoptium)"
+    date = "2026-06-30T09:16:57-0400",
+    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
 )
 @Component
 public class IProductoMapperImpl implements IProductoMapper {
